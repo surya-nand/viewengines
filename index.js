0f61259a43f8fb9cf38bf6649dc044eb8b7b5ffb
surya-nand <83575893+surya-nand@users.noreply.github.com>
2023-05-27 20:36:07 +0530

    view-engine working

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static('./public'))
+
+app.set('view engine','ejs')
 app.get('/',(req,res) =>{
     res.send('Working')
 })
 
-app.get('/user-details',(req,res)=>{
-    res.render('index.ejs')
+app.get('/user-details/Surya',(req,res)=>{
+    res.render('user-details',{firstname:'Surya',BatchNum: 3,year: 2023, passout:2022})
 })
+
+app.get('/user-details/Anand',(req,res)=>{
+    res.render('user-details',{firstname:'Anand',BatchNum: 5,year: 2020, passout:2000})
+})
+
+
 app.listen(700,()=>{
     console.log("Server is ruuning on james bond port")
 })
